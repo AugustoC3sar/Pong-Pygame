bf3e8eabe3c24d8e118264936be14219b64da092
AugustoC3sar <c.augusto0302@gmail.com>
2021-07-23 21:05:05 -0300

    Players movement, Ball movement and some details

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pygame
+from pygame.constants import *
+import random
 
 
-WIDTH, HEIGHT = 1080, 500
+WIDTH, HEIGHT = 900, 500
 FPS = 60
 PLAYER_WIDTH = 20
-PLAYER_HEIGHT = 50
+PLAYER_HEIGHT = 80
 BALL_DIMENSION = 20
-
+MOVE_VEL = 5
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+DIRECTIONS = ('TOP-RIGHT','BOTTOM-RIGHT','TOP-LEFT','BOTTOM-LEFT')
+game_speed = 3
 
 
 class Engine:
@@ -14,9 +20,50 @@ class Engine:
         self.__player1 = Player(10)
         self.__player2 = Player(WIDTH-PLAYER_WIDTH-10)
         self.__ball = Ball()
+        self.__root = pygame.display.set_mode((WIDTH, HEIGHT))
         self.__run = True
         self.__end = False
 
+    def game_loop(self):
+        clock = pygame.time.Clock()
+        while self.__run:
+            clock.tick(FPS)
+            self.__root.fill(BLACK)
+            if self.__end:
+                pass
+            else:
+                self.draw_ball()
+                self.draw_players()
+                self.draw_line()
+                self.get_key()
+                self.__ball.move()
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.__run = False
+            
+            pygame.display.update()
+
+    def draw_players(self):
+        pygame.draw.rect(self.__root, WHITE, self.__player1.hitbox)
+        pygame.draw.rect(self.__root, WHITE, self.__player2.hitbox)
+
+    def draw_ball(self):
+        pygame.draw.rect(self.__root, WHITE, self.__ball.hitbox)
+
+    def draw_line(self):
+        pygame.draw.line(self.__root, WHITE, (WIDTH/2,0), (WIDTH/2,HEIGHT), 1)
+
+    def get_key(self):
+        keys = pygame.key.get_pressed()
+        if keys[K_w]:
+            self.__player1.move_up()
+        elif keys[K_s]:
+            self.__player1.move_down()
+        if keys[K_UP]:
+            self.__player2.move_up()
+        elif keys[K_DOWN]:
+            self.__player2.move_down()
 
 class Player:
     def __init__(self, x):
@@ -52,11 +99,22 @@ class Player:
     def update_hitbox(self):
         self.__hitbox.update(self.__x, self.__y, PLAYER_WIDTH, PLAYER_HEIGHT)
 
+    def move_up(self):
+        if self.__y > 10:
+            self.__y -= MOVE_VEL
+            self.update_hitbox()
+
+    def move_down(self):
+        if self.__y < HEIGHT-PLAYER_HEIGHT-10:
+            self.__y += MOVE_VEL
+            self.update_hitbox()
+
 
 class Ball:
     def __init__(self):
-        self.__x = WIDTH/2 - BALL_DIMENSION
-        self.__y = HEIGHT/2 - BALL_DIMENSION
+        self.__x = WIDTH/2 - BALL_DIMENSION/2
+        self.__y = HEIGHT/2 - BALL_DIMENSION/2
+        self.__direction = random.choice(DIRECTIONS)
         self.__hitbox = pygame.Rect(self.__x, self.__y, BALL_DIMENSION, BALL_DIMENSION)
 
     @property
@@ -76,11 +134,39 @@ class Ball:
         self.__y = new_y
 
     @property
+    def direction(self):
+        return self.__direction
+
+    @property
     def hitbox(self):
         return self.__hitbox
 
     def update_hitbox(self):
         self.__hitbox = pygame.Rect(self.__x, self.__y, BALL_DIMENSION, BALL_DIMENSION)
 
+    def move(self):
+        if self.__direction == 'TOP-RIGHT':
+            self.__x += game_speed
+            self.__y -= game_speed
+            if self.__y == 0:
+                self.__direction = 'BOTTOM-RIGHT'
+        elif self.__direction == 'BOTTOM-RIGHT':
+            self.__x += game_speed
+            self.__y += game_speed
+            if self.__y == HEIGHT-BALL_DIMENSION:
+                self.__direction = 'TOP-RIGHT'
+        elif self.__direction == 'TOP-LEFT':
+            self.__x -= game_speed
+            self.__y -= game_speed
+            if self.__y == 0:
+                self.__direction = 'BOTTOM-LEFT'
+        else:
+            self.__x -= game_speed
+            self.__y += game_speed
+            if self.__y == HEIGHT-BALL_DIMENSION:
+                self.__direction = 'TOP-LEFT'
+        self.update_hitbox()
+
 
 game = Engine()
+game.game_loop()
