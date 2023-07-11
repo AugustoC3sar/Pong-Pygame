e2a7b9712e7cd0e5e7418ca45faa38888aa965ff
César Augusto <c.augusto0302@gmail.com>
2023-07-10 21:50:51 -0300

    Game Settings

---
+++ config.py
@@ -0,0 +1,28 @@
+import pygame
+
+# Screen Size
+WIDTH, HEIGHT = 700, 500
+
+# Font
+pygame.font.init()
+FONT = pygame.font.Font('PressStart2P.ttf', 20)
+
+# Window update ratio
+FPS = 60
+
+# Player Size
+P_WIDTH, P_HEIGHT = 20, 80
+
+# Player Movement Velocity
+MOVE_VEL = 5
+
+# Ball size
+BALL_SIZE = 20
+
+# Colors
+WHITE = (255, 255, 255)
+BG_COLOR = (10,0,30)
+
+# Limit score
+WIN = 5
+
